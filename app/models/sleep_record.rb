5166c9d733214e7baa1e99c25bbde9a8106021c9
PeiTung Liao <ym951224@gmail.com>
2025-08-06 23:06:19 +0000

    Add sleep_record validate and comment

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -1,3 +1,14 @@
 class SleepRecord < ApplicationRecord
   belongs_to :user
+
+  validate :wake_up_after_sleep
+
+  private
+
+  def wake_up_after_sleep
+    return if wake_up_at.blank? || sleep_at.blank?
+    if wake_up_at <= sleep_at
+      errors.add(:wake_up_at, "must be after sleep_at")
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   has_many :sleep_records, dependent: :destroy
 
+  # The Follow records where this user is the follower
   has_many :follows, foreign_key: :follower_id
   has_many :followees, through: :follows
 
+  # The Follow records where this user is being followed
   has_many :reverse_follows, class_name: 'Follow', foreign_key: :followee_id
   has_many :followers, through: :reverse_follows, source: :follower
 end
